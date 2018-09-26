9c6d67a5f7844008548ee85160d88f4df84f3377
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-09-26 23:16:00 +0800

    Mapper接口实现ExampleMapper<T>，相比ConditionMapper<T>更加方便进行条件查询

--- spring-boot-quick-project-fars/src/main/java/com/huijava/common/base/Mapper.java
+++ spring-boot-quick-project-fars/src/main/java/com/huijava/common/base/Mapper.java
@@ -2,6 +2,7 @@ package com.huijava.common.base;
 
 import tk.mybatis.mapper.common.BaseMapper;
 import tk.mybatis.mapper.common.ConditionMapper;
+import tk.mybatis.mapper.common.ExampleMapper;
 import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.special.InsertListMapper;
 
@@ -13,5 +14,6 @@ public interface Mapper<T>
         BaseMapper<T>,
         ConditionMapper<T>,
         IdsMapper<T>,
+        ExampleMapper<T>,
         InsertListMapper<T> {
 }
